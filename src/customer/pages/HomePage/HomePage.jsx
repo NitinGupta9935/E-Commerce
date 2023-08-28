57252cd973b66e92e30f8ec4fd0f2f6200bf5230
Nitin Gupta <guptanitin493@gmail.com>
2023-08-28 19:09:09 +0530

    28.08.2023

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../HomeCarosel/MainCrosel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
