e73d3d4cef1aa9b5a4866803f516f7a9878ce564
Nitin Gupta <guptanitin493@gmail.com>
2023-08-29 19:28:20 +0530

    add kurta pics

---
+++ src/customer/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import AliceCarousel from "react-alice-carousel";
+import { Button } from "@mui/material";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+
+const HomeSectionCarosel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({item}) => setActiveIndex(item); 
+
+  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+
+  return (
+    <div className="border ">
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls
+        />
+        {activeIndex !== items.length - 5 && <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slideNext}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>}
+
+        <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slidePrev}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0rem",
+            transform: "translateX(-50%) rotate(-90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import MainCarousel from '../../HomeCarosel/MainCrosel'
+import HomeSectionCarosel from '../../HomeSectionCarosel/HomeSectionCarosel'
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
-        <div>
-            other section
+        <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
+            <HomeSectionCarosel />
         </div>
     </div>
   )
